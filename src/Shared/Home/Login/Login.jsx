bfd3a7a0d3bfe0ad08c3441147d167f0db425de0
unknown <personal.abusaid@gmail.com>
2023-05-03 16:58:15 +0600

    handle google and github popup

--- src/Shared/Home/Login/Login.jsx
+++ src/Shared/Home/Login/Login.jsx
@@ -2,28 +2,58 @@ import { Button, Container, Form } from "react-bootstrap";
 import './Login.css'
 import { Link } from "react-router-dom";
 import { FaGoogle, FaGithub } from 'react-icons/fa';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../../firebase/firebase.config";
+
 
 const Login = () => {
+
+    const auth = getAuth(app);
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+    const handleGoogleSignIN = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+            
+        }    
+        const handleGithubSignIn=()=>{
+            signInWithPopup(auth,githubProvider)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+    }
     return (
         <Container className="login-form">
-           <Form className="w-75 mx-auto my-2" >
-      <Form.Group className="mb-3" controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control  type="email" name="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Password" />
-      </Form.Group>
-     <div>
-     <Button className="mb-2" variant="outline-warning" size='lg' active>Login</Button>
-     </div>
-      
-      <Button className="mb-2 " variant="outline-primary" size="lg">  <FaGoogle /> Login With Google</Button> 
-      <Button variant="outline-success" size="lg"> <FaGithub /> Login With GitHub</Button>
-      <p>New to Bengali Kitchen ?<Link to='/register'>Register here</Link></p>
-      
-    </Form>
+            <Form className="w-75 mx-auto my-2" >
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" />
+                </Form.Group>
+                <div>
+                    <Button className="mb-2" variant="outline-warning" size='lg' active>Login</Button>
+                </div>
+
+                <Button onClick={handleGoogleSignIN} className="mb-2 " variant="outline-primary" size="lg">  <FaGoogle /> Login With Google</Button>
+                <Button onClick={handleGithubSignIn} variant="outline-success" size="lg"> <FaGithub /> Login With GitHub</Button>
+                <p>New to Bengali Kitchen ?<Link to='/register'>Register here</Link></p>
+
+            </Form>
         </Container>
     );
 };
--- src/Shared/Home/Register/Register.jsx
+++ src/Shared/Home/Register/Register.jsx
@@ -3,29 +3,43 @@ import { Link } from "react-router-dom";
 
 
 const Register = () => {
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+    }
+
     return (
-        <Container>
-            <h2> PLease Register Here</h2>
+        <div>
+            <h2 className="text-center mt-2"> PLease Register Here</h2>
             <Container className="login-form">
-           <Form className="w-75 mx-auto my-2" >
-      <Form.Group className="mb-3" controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control  type="email" name="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Password" />
-      </Form.Group>
-     <div>
-     <Button className="mb-2" variant="outline-warning" size='lg' active>Login</Button>
-     </div>
-      
-      
-      <p>Already have an account ?<Link to='/login'>Login here</Link></p>
-      
-    </Form>
-        </Container>
-        </Container>
+                <form onSubmit={handleRegister} className="w-75 mx-auto my-2" >
+                    <Form.Group className="mb-3" controlId="formGroupName">
+                        <Form.Label> Name</Form.Label>
+                        <Form.Control type="text" name="name" placeholder="Enter Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" name="email" placeholder="Enter email" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name="password" placeholder="Password" required />
+                    </Form.Group>
+
+                    <div>
+                        <Button className="mb-2" variant="outline-warning" size='lg' active>Register</Button>
+                    </div>
+
+
+                    <p>Already have an account ?<Link to='/login'>Login here</Link></p>
+
+                </form>
+            </Container>
+        </div>
     );
 };
 
