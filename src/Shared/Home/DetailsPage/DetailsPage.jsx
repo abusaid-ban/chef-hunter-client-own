204a2f3c723f7f63b9bc24dac43ba6f20d7cd276
unknown <personal.abusaid@gmail.com>
2023-05-05 12:09:58 +0600

    create table in the details page

--- src/Shared/Home/DetailsPage/DetailsPage.jsx
+++ src/Shared/Home/DetailsPage/DetailsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Table } from 'react-bootstrap';
 import { useLoaderData, useParams } from 'react-router-dom';
 import './DetailsPages.css'
 
@@ -13,12 +13,13 @@ const DetailsPage = () => {
         console.log(chefData);
         setDetails(chefData);
     }, [])
-    return (
+    
+   return (
         <Container className='my-5'>
             <h2 className='text-center'>Chef Details</h2>
-                <div className='details'>
+            <div className='details'>
                 <div>
-                   <img src={details.picture} alt="" />
+                    <img src={details.picture} alt="" />
                 </div>
                 <div>
                     <h5>Chef Name: {details.name}</h5>
@@ -27,8 +28,40 @@ const DetailsPage = () => {
                     <h5>Number of Recipe: {details.recipes}</h5>
                     <h5>Experience: {details.experience} Years</h5>
                 </div>
-                </div>
-                <table class="table-success">...</table>
+            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Recipe Name</th>
+                        <th scope="col">Ingredients</th>
+                        <th scope="col">Cooking Method</th>
+                        <th scope="col">Rating</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <tr>
+                        <th scope="row">{details.recipe_name}</th>
+                        <td>{details.ingredients}</td>
+                        <td>{details.method}</td>
+                        <td>{details.rating}</td>
+                        <td> <Button variant="danger">Favorite</Button></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">{details.recipe_name2}</th>
+                        <td>{details.ingredients}</td>
+                        <td>{details.method}</td>
+                        <td>{details.rating2}</td>
+                        <td> <Button variant="danger">Favorite</Button></td>
+                    </tr>
+                    <tr>
+                    <th scope="row">{details.recipe_name3}</th>
+                        <td>{details.ingredients}</td>
+                        <td>{details.method}</td>
+                        <td>{details.rating3}</td>
+                        <td> <Button variant="danger">Favorite</Button></td>
+                    </tr>
+                </tbody>
+            </table>
         </Container>
     );
 };
